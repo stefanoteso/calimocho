9e2336c4ea045c17228bb4d9fc6234d2fca7588e
Stefano Teso <stefano.teso@gmail.com>
2019-05-27 11:46:45 +0200

    xor: reduce the number of cases.

--- xor.sh
+++ xor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-for arch in "-W 3 3"; do
-    for lambda2 in 1 0.1 0.01 0; do
-        for lambda1 in 1 0.1 0.01 0; do
+for arch in "-W 3" "-W 3 3"; do
+    for lambda2 in 1 0.1 0; do
+        for lambda1 in 1 0.1 0; do
             python main.py xor full_full --passive -k 5 -p 0.2 -E 1000 \
                 $arch --lambda1 $lambda1 --lambda2 $lambda2 --use-corrections
         done
