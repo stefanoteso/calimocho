5671477286f15a5fab1d512ba25f51d430e6481a
Stefano Teso <stefano.teso@gmail.com>
2019-06-10 11:24:30 +0200

    colors: update script.

--- colors.sh
+++ colors.sh
@@ -2,11 +2,9 @@
 
 for exp in colors0 colors1; do
     for arch in "-W 101" "-W 101 101"; do
-        for lambda2 in 1 0.1 0; do
-            for lambda1 in 1 0.1 0; do
-                python main.py $exp full_full --passive -k 5 -p 0.2 -E 1000 \
-                    $arch --lambda1 $lambda1 --lambda2 $lambda2 --use-corrections
-            done
+        for lambdas in "1 0" "0.9 0" "0.5 0" "0.1 0"; do
+            python main.py $exp full_full --passive -k 5 -p 0.2 \
+                $arch -E 1000 --lambdas $lambdas
         done
     done
 done
