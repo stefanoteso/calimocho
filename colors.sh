d6d999324256ec0904dfa7fba2d6ad9ef741bad9
Stefano Teso <stefano.teso@gmail.com>
2019-06-11 15:18:30 +0200

    colors: update the runner script.

--- colors.sh
+++ colors.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for exp in colors1; do
+for exp in colors0 colors1; do
     for arch in "101" "101 101" "101 101 101" "101 101 101 101" " 101 101 101 101 101"; do
         for lambdas in "1 0" "0.9 0" "0.5 0" "0.1 0" "0 0"; do
-            python main.py $exp full_full --passive -k 1 -p 0.2 \
+            python main.py $exp full_full --passive -k 5 -p 0.2 \
                 -W $arch -E 1000 --lambdas $lambdas -e 0.01
         done
     done
