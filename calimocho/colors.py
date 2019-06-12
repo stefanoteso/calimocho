3d9b5de014e5ec0b9a304ce78ac1151fd5e21a1a
Stefano Teso <stefano.teso@gmail.com>
2019-06-12 11:51:55 +0200

    colors: make LIME work.

--- calimocho/colors.py
+++ calimocho/colors.py
@@ -6,11 +6,12 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.patches import Circle
 
+from collections import defaultdict
 from lime.lime_tabular import LimeTabularExplainer
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Ridge
 
-from . import Experiment, PipeStep
+from . import Experiment, PipeStep, load, dump
 
 
 _TO_OHE = {
@@ -20,6 +21,7 @@ _TO_OHE = {
     (128,   0, 255): (1, 0, 0, 0), # v
 }
 _TO_RAW = {v: k for k, v in _TO_OHE.items()}
+_COLORS_RGB = tuple(_TO_OHE.keys())
 
 
 class ColorsExperiment(Experiment):
@@ -27,31 +29,52 @@ class ColorsExperiment(Experiment):
     def __init__(self, **kwargs):
         self.rule = kwargs.pop('rule')
 
-        data = np.load(join('data', 'toy_colors.npz'))
-        images = np.vstack([data['arr_0'], data['arr_1']])
+        pp_path = join('data', 'toy_colors_pp.pickle')
+        try:
+            X, Z, y, flat_images = load(pp_path)
+        except:
+            data = np.load(join('data', 'toy_colors.npz'))
+            raw_images = np.vstack([data['arr_0'], data['arr_1']])
+            n_examples = len(raw_images)
 
-        X = np.array([self._raw_to_ohe(x.reshape(5, 5, 3)).ravel() for x in images])
-        bias = np.ones((X.shape[0], 1))
-        X = np.hstack([X, bias])
-        Z = np.vstack([self._w_star(x) for x in X])
-        y = 1 - np.hstack((data['arr_2'], data['arr_3']))
+            flat_images = np.array([self._raw_to_flat(ri.reshape(5, 5, 3))
+                                    for ri in raw_images])
+            X = np.array([self._raw_to_ohe(ri.reshape(5, 5, 3)).ravel()
+                          for ri in raw_images])
+            X = np.hstack([X, np.ones((n_examples, 1))])
+            Z = np.vstack([self._w_star(x) for x in X])
+            y = 1 - np.hstack((data['arr_2'], data['arr_3']))
 
-        # DEBUG
-        #y2 = np.einsum('bi,bi->b', X, Z)
-        #print(np.hstack([y.reshape(-1, 1), y2.reshape(-1, 1)]))
-        #quit()
+            # DEBUG
+            #y2 = np.einsum('bi,bi->b', X, Z)
+            #print(np.hstack([y.reshape(-1, 1), y2.reshape(-1, 1)]))
+            #quit()
+
+            dump(pp_path, [X, Z, y, flat_images])
+
+        self.flat_images = flat_images
 
         super().__init__(X, Z, y, **kwargs)
 
     @staticmethod
-    def _raw_to_ohe(x):
-        ohe = [_TO_OHE[tuple(x[r, c])] for r, c in product(range(5), repeat=2)]
+    def _raw_to_ohe(ri):
+        ohe = [_TO_OHE[tuple(ri[r, c])] for r, c in product(range(5), repeat=2)]
         return np.array(ohe).reshape(5, 5, 4)
 
     @staticmethod
-    def _ohe_to_raw(x):
-        raw = [_TO_RAW[tuple(x[r, c])] for r, c in product(range(5), repeat=2)]
+    def _ohe_to_raw(ri):
+        raw = [_TO_RAW[tuple(ri[r, c])] for r, c in product(range(5), repeat=2)]
         return np.array(raw).reshape(5, 5, 3)
+
+    @staticmethod
+    def _raw_to_flat(ri):
+        fi = [_COLORS_RGB.index(tuple(ri[r, c])) for r, c in product(range(5), repeat=2)]
+        return np.array(fi)
+
+    @staticmethod
+    def _flat_to_ohe(fi):
+        ohe = [_TO_OHE[_COLORS_RGB[fi[r,c]]] for r, c in product(range(5), repeat=2)]
+        return np.array(ohe).ravel()
 
     @staticmethod
     def rule0(x):
@@ -125,44 +148,48 @@ class ColorsExperiment(Experiment):
                      model,
                      known_examples,
                      target_example,
-                     n_repeats=1,
+                     n_repeats=5,
                      n_samples=100,
                      n_features=4,
-                     metric='euclidean'):
-        lime = LimeTabularExplainer(self.Z[known],
+                     metric='euclidean',
+                     kernel_width=1.0):
+        CLASS_NAMES = ['negative', 'positive']
+        FEATURE_NAMES = ['{r}_{c}'.format(**locals())
+                         for r, c in product(range(5), repeat=2)]
+        FEATURES = list(range(len(FEATURE_NAMES)))
+
+        lime = LimeTabularExplainer(self.flat_images[known_examples],
                                     class_names=CLASS_NAMES,
                                     feature_names=FEATURE_NAMES,
-                                    kernel_width=1.0,
-                                    categorical_featres=list(range(FEATURE_NAMES)),
+                                    categorical_features=FEATURES,
                                     discretize_continuous=False,
                                     feature_selection='forward_selection',
+                                    kernel_width=kernel_width,
                                     verbose=True)
 
-        step = PipeStep(lambda Z: np.array([self._ohe_to_raw(z).ravel() for z in Z],
-                                           dtype=np.float64))
-        pipeline = make_pipeline(step, model)
+        def flat_to_x(flat_images):
+            n_examples = len(flat_images)
+            X = np.array([self._flat_to_ohe(fi.reshape(5, 5)) for fi in flat_images.astype(int)],
+                         dtype=np.float32)
+            return np.hstack([X, np.ones((n_examples, 1))])
 
-        try:
-            counts = defaultdict(int)
-            for _ in range(n_repeats):
-                local_model = Ridge(alpha=1, fit_intercept=True, random_state=0)
-                explanation = lime.explain_instance(self.Z[target_example],
-                                                    pipeline.predict_proba,
-                                                    model_regressor=local_model,
-                                                    n_samples=n_samples,
-                                                    n_features=n_features,
-                                                    distance_metric=metric)
-                for feat, coeff in explanation.as_list():
-                    coeff = int(np.sign(coeff))
-                    counts[(feat, coeff)] += 1
+        pipeline = make_pipeline(PipeStep(flat_to_x), model)
 
-            sorted_counts = sorted(counts.items(), key=lambda _: _[-1])
-            sorted_counts = list(sorted_counts)[-n_features:]
-            return [fs for fs, _ in sorted_counts]
+        local_model = Ridge(alpha=1, fit_intercept=True, random_state=0)
 
-        except FloatingPointError:
-            # XXX sometimes the calibrator classifier CV throws this
-            print('Warning: LIME failed, returning no explanation')
-            return None
+        counts = defaultdict(int)
+        for _ in range(n_repeats):
+            explanation = lime.explain_instance(self.flat_images[target_example],
+                                                pipeline.predict_proba,
+                                                model_regressor=local_model,
+                                                num_samples=n_samples,
+                                                num_features=n_features,
+                                                distance_metric=metric)
+            print(explanation.as_list())
+            for feat, coeff in explanation.as_list():
+                coeff = int(np.sign(coeff))
+                counts[(feat, coeff)] += 1
 
-        return sorted_counts
+        sorted_counts = sorted(counts.items(), key=lambda _: _[-1])
+        sorted_counts = list(sorted_counts)[-n_features:]
+        return [fs for fs, _ in sorted_counts]
