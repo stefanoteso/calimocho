3b6304f5440756aad00538c9471583d97238ea61
Stefano Teso <stefano.teso@gmail.com>
2019-06-14 10:08:26 +0200

    colors: update runner script for last batch of experiments.

--- colors.sh
+++ colors.sh
@@ -25,20 +25,36 @@
 #    done
 #done
 
-# Active learning
-for exp in colors0 colors1; do
-    for strategy in random margin; do
-        for arch in "101" "101 101 101" "101 101 101 101 101"; do
-            for lambdas in "1 0" "0.9 0" "0.9 0.1" "0.5 0" "0.5 0.1" "0.1 0" "0.1 0.1" "0 0" "0 0.1"; do
-                python main.py $exp full_full \
-                    --strategy $strategy -k 5 -p 0.0001 -T 300 \
-                    -W $arch -E 100 --lambdas $lambdas -e 0.01
-            done
+# # Active learning
+# for exp in colors0 colors1; do
+#     for strategy in random margin; do
+#         for arch in "101" "101 101 101" "101 101 101 101 101"; do
+#             for lambdas in "1 0" "0.9 0" "0.9 0.1" "0.5 0" "0.5 0.1" "0.1 0" "0.1 0.1" "0 0" "0 0.1"; do
+#                 python main.py $exp full_full \
+#                     --strategy $strategy -k 5 -p 0.0001 -T 300 \
+#                     -W $arch -E 100 --lambdas $lambdas -e 0.01
+#             done
+#
+#             fmtarch=`echo $arch | tr ' ' ','`
+#             python draw.py \
+#                 $exp-active-W=$fmtarch-$strategy \
+#                 results/${exp}__strategy=${strategy}__passive=False__*__W=${fmtarch}__*__L=*,0.0__*.pickle
+#         done
+#     done
+# done
 
-            fmtarch=`echo $arch | tr ' ' ','`
-            python draw.py \
-                $exp-active-W=$fmtarch-$strategy \
-                results/${exp}__strategy=${strategy}__passive=False__*__W=${fmtarch}__*__L=*,0.0__*.pickle
+# Active learning from partial explanations
+for exp in colors0; do
+    for strategy in random; do
+        for arch in "101"; do
+            for lambdas in "1 0" "0 0"; do
+                for prop_corr in 0.9 0.5 0.1; do
+                    python main.py $exp full_full \
+                        --strategy $strategy -k 5 -p 0.0001 -T 300 \
+                        -W $arch -E 100 --lambdas $lambdas -e 0.01 \
+                        -c $prop_corr
+                done
+            done
         done
     done
 done
